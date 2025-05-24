7d4f5f049c88aa8aa68a46bba75927e0898f96f2
alt-0191 <2223147307@qq.com>
2025-05-25 04:55:53 +0800

    Rewrite TinyUSB part for multiple USB interface

--- usb/usb_descriptors.c
+++ usb/usb_descriptors.c
@@ -1,239 +1,164 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
 #include "bsp/board_api.h"
 #include "tusb.h"
 #include "usb_descriptors.h"
 
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]         HID | MSC | CDC          [LSB]
- */
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x0001 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
-
-#define USB_VID   0x5048
-#define USB_BCD   0x0200
-
 //--------------------------------------------------------------------+
-// Device Descriptors
+// Device Descriptors - Core USB device properties
 //--------------------------------------------------------------------+
 tusb_desc_device_t const desc_device =
+		{
+				.bLength = sizeof(tusb_desc_device_t),
+				.bDescriptorType = TUSB_DESC_DEVICE,
+				.bcdUSB = USB_BCD,
+				.bDeviceClass = 0x00,
+				.bDeviceSubClass = 0x00,
+				.bDeviceProtocol = 0x00,
+				.bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+
+				.idVendor = USB_VID,
+				.idProduct = USB_PID,
+				.bcdDevice = 0x0100,
+
+				.iManufacturer = 0x01,
+				.iProduct = 0x02,
+				.iSerialNumber = 0x03,
+
+				.bNumConfigurations = 0x01};
+
+// Return device descriptor when requested by host
+uint8_t const *tud_descriptor_device_cb(void)
 {
-    .bLength            = sizeof(tusb_desc_device_t),
-    .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = USB_BCD,
-    .bDeviceClass       = 0x00,
-    .bDeviceSubClass    = 0x00,
-    .bDeviceProtocol    = 0x00,
-    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-
-    .idVendor           = USB_VID,
-    .idProduct          = USB_PID,
-    .bcdDevice          = 0x0100,
-
-    .iManufacturer      = 0x01,
-    .iProduct           = 0x02,
-    .iSerialNumber      = 0x03,
-
-    .bNumConfigurations = 0x01
-};
-
-// Invoked when received GET DEVICE DESCRIPTOR
-// Application return pointer to descriptor
-uint8_t const * tud_descriptor_device_cb(void)
-{
-  return (uint8_t const *) &desc_device;
+	return (uint8_t const *)&desc_device;
 }
 
 //--------------------------------------------------------------------+
-// HID Report Descriptor
+// HID Report Descriptors - Define HID device types
 //--------------------------------------------------------------------+
 
-uint8_t const desc_hid_report[] =
-    {
-        TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD)),
-        TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(REPORT_ID_MOUSE)),
-        TUD_HID_REPORT_DESC_GAMEPAD(HID_REPORT_ID(REPORT_ID_GAMEPAD)),
-        TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE,HID_REPORT_ID(REPORT_ID_GENERIC_INOUT)),};
+// Keyboard HID report descriptor
+uint8_t const desc_hid_keyboard[] =
+		{
+				TUD_HID_REPORT_DESC_KEYBOARD()};
 
-// Invoked when received GET HID REPORT DESCRIPTOR
-// Application return pointer to descriptor
-// Descriptor contents must exist long enough for transfer to complete
-uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance)
+// Mouse HID report descriptor
+uint8_t const desc_hid_mouse[] =
+		{
+				TUD_HID_REPORT_DESC_MOUSE()};
+
+// Gamepad HID report descriptor
+uint8_t const desc_hid_gamepad[] =
+		{
+				TUD_HID_REPORT_DESC_GAMEPAD()};
+
+// RAWHID descriptor
+uint8_t const desc_hid_rawhid[] =
+		{
+				TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE)};
+
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf)
 {
-  (void) instance;
-  return desc_hid_report;
+	if (itf == 0)
+	{
+		return desc_hid_keyboard;
+	}
+	else if (itf == 1)
+	{
+		return desc_hid_mouse;
+	}
+	else if (itf == 2)
+	{
+		return desc_hid_gamepad;
+	}
+	else if (itf == 3)
+	{
+		return desc_hid_rawhid;
+	}
+
+	return NULL;
 }
-
 //--------------------------------------------------------------------+
-// Configuration Descriptor
+// Configuration Descriptor - Interface/Endpoint setup
 //--------------------------------------------------------------------+
 
-enum
-{
-  ITF_NUM_HID,
-  ITF_NUM_TOTAL
-};
-
-#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN)
-
-#define EPNUM_HID   0x01
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN + TUD_HID_DESC_LEN + TUD_HID_DESC_LEN + TUD_HID_INOUT_DESC_LEN)
 
 uint8_t const desc_configuration[] =
-    {
-        // Config number, interface count, string index, total length, attribute, power in mA
-        TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+		{
+				// Config number, interface count, string index, total length, attribute, power in mA
+				TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
-        // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
-   TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, 0x80 | EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 10)
-};
-
-#if TUD_OPT_HIGH_SPEED
-// Per USB specs: high speed capable device must report device_qualifier and other_speed_configuration
-
-// other speed configuration
-uint8_t desc_other_speed_config[CONFIG_TOTAL_LEN];
-
-// device qualifier is mostly similar to device descriptor since we don't change configuration based on speed
-tusb_desc_device_qualifier_t const desc_device_qualifier =
-{
-  .bLength            = sizeof(tusb_desc_device_qualifier_t),
-  .bDescriptorType    = TUSB_DESC_DEVICE_QUALIFIER,
-  .bcdUSB             = USB_BCD,
-
-  .bDeviceClass       = 0x00,
-  .bDeviceSubClass    = 0x00,
-  .bDeviceProtocol    = 0x00,
-
-  .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-  .bNumConfigurations = 0x01,
-  .bReserved          = 0x00
-};
-
-// Invoked when received GET DEVICE QUALIFIER DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete.
-// device_qualifier descriptor describes information about a high-speed capable device that would
-// change if the device were operating at the other speed. If not highspeed capable stall this request.
-uint8_t const* tud_descriptor_device_qualifier_cb(void)
-{
-  return (uint8_t const*) &desc_device_qualifier;
-}
-
-// Invoked when received GET OTHER SEED CONFIGURATION DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-// Configuration descriptor in the other speed e.g if high speed then this is for full speed and vice versa
-uint8_t const* tud_descriptor_other_speed_configuration_cb(uint8_t index)
-{
-  (void) index; // for multiple configurations
-
-  // other speed config is basically configuration with type = OHER_SPEED_CONFIG
-  memcpy(desc_other_speed_config, desc_configuration, CONFIG_TOTAL_LEN);
-  desc_other_speed_config[1] = TUSB_DESC_OTHER_SPEED_CONFIG;
-
-  // this example use the same configuration for both high and full speed mode
-  return desc_other_speed_config;
-}
-
-#endif // highspeed
+				// Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
+				TUD_HID_DESCRIPTOR(INTERFACE_KEYBOARD, 4, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_keyboard), EPNUM_KEYBOARD, CFG_TUD_HID_EP_BUFSIZE, USB_POLLING_INTERVAL),
+				TUD_HID_DESCRIPTOR(INTERFACE_MOUSE, 5, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_mouse), EPNUM_MOUSE, CFG_TUD_HID_EP_BUFSIZE, USB_POLLING_INTERVAL),
+				TUD_HID_DESCRIPTOR(INTERFACE_GAMEPAD, 6, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_gamepad), EPNUM_GAMEPAD, CFG_TUD_HID_EP_BUFSIZE, USB_POLLING_INTERVAL),
+				TUD_HID_INOUT_DESCRIPTOR(INTERFACE_RAWHID, 7, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_rawhid), EPNUM_RAWHID, 0x80 | EPNUM_RAWHID, CFG_TUD_HID_EP_BUFSIZE, USB_POLLING_INTERVAL)};
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
 {
-  (void) index; // for multiple configurations
-
-  // This example use the same configuration for both high and full speed mode
-  return desc_configuration;
+	(void)index; // for multiple configurations
+	return desc_configuration;
 }
 
 //--------------------------------------------------------------------+
 // String Descriptors
 //--------------------------------------------------------------------+
 
-// String Descriptor Index
-enum {
-  STRID_LANGID = 0,
-  STRID_MANUFACTURER,
-  STRID_PRODUCT,
-  STRID_SERIAL,
-};
-
-// array of pointer to string descriptors
+// String descriptors (index 1: Manufacturer, 2: Product, 6: PHAC, 7: Interface)
 char const *string_desc_arr[] =
-{
-  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "PHDesign",                     // 1: Manufacturer
-  "PHDesign PHAC",              // 2: Product
-  NULL,                          // 3: Serials will use unique ID if possible
-};
+		{
+				(const char[]){0x09, 0x04}, // 0: Supported language is English (0x0409)
+				"PHDesign",									// 1: Manufacturer
+				"PHDesign PHAC_V1",					// 2: Product
+				NULL,												// 3: Serial Number (unused)
+				NULL,												// 4: Keyboard Interface (unused)
+				NULL,												// 5: Mouse Interface (unused)
+				"PHAC Controller",					// 6: Gamepad Interface It's needed to show the ture name
+				"PHDesign PHAC Interface"}; // 7: RAWHID Interface For Button Mapping
 
 static uint16_t _desc_str[32 + 1];
 
-// Invoked when received GET STRING DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
-  (void) langid;
-  size_t chr_count;
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
+	(void)langid;
+	size_t chr_count;
 
-  switch ( index ) {
-    case STRID_LANGID:
-      memcpy(&_desc_str[1], string_desc_arr[0], 2);
-      chr_count = 1;
-      break;
+	switch (index)
+	{
+	case STRID_LANGID:
+		memcpy(&_desc_str[1], string_desc_arr[0], 2);
+		chr_count = 1;
+		break;
 
-    case STRID_SERIAL:
-      chr_count = board_usb_get_serial(_desc_str + 1, 32);
-      break;
+	case STRID_SERIAL:
+		chr_count = board_usb_get_serial(_desc_str + 1, 32);
+		break;
 
-    default:
-      // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-      // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+	default:
 
-      if ( !(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])) ) return NULL;
+		if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
+			return NULL;
 
-      const char *str = string_desc_arr[index];
+		const char *str = string_desc_arr[index];
 
-      // Cap at max char
-      chr_count = strlen(str);
-      size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
-      if ( chr_count > max_count ) chr_count = max_count;
+		// Cap at max char
+		chr_count = strlen(str);
+		size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
+		if (chr_count > max_count)
+			chr_count = max_count;
 
-      // Convert ASCII string into UTF-16
-      for ( size_t i = 0; i < chr_count; i++ ) {
-        _desc_str[1 + i] = str[i];
-      }
-      break;
-  }
+		// Convert ASCII string into UTF-16
+		for (size_t i = 0; i < chr_count; i++)
+		{
+			_desc_str[1 + i] = str[i];
+		}
+		break;
+	}
 
-  // first byte is length (including header), second byte is string type
-  _desc_str[0] = (uint16_t) ((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
+	// first byte is length (including header), second byte is string type
+	_desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
 
-  return _desc_str;
+	return _desc_str;
 }
--- usb/usb_descriptors.h
+++ usb/usb_descriptors.h
@@ -1,40 +1,71 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 #ifndef USB_DESCRIPTORS_H_
 #define USB_DESCRIPTORS_H_
 
-#define EPNUM_HID_KEYBOARD   0x81 // 键盘使用端点0x81
-#define EPNUM_HID_MOUSE      0x82 // 鼠标使用端点0x82
+//--------------------------------------------------------------------+
+// Macro Constants
+//--------------------------------------------------------------------+
+
+#define USB_VID 0x5048
+#define USB_BCD 0x0200
+
+#define _PID_MAP(itf, n) ((CFG_TUD_##itf) << (n))
+#define USB_PID (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
+				 _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4))
+
+#define EPNUM_KEYBOARD 0x81
+#define EPNUM_MOUSE 0x82
+#define EPNUM_GAMEPAD 0x83
+#define EPNUM_RAWHID 0x04
+#define USB_POLLING_INTERVAL 1 // Do not modify - knob filtering algorithm depends on this
+
+//--------------------------------------------------------------------+
+// Enums
+//--------------------------------------------------------------------+
 
 enum
 {
-  REPORT_ID_KEYBOARD = 1,
-  REPORT_ID_MOUSE,
-  REPORT_ID_GAMEPAD,
-  REPORT_ID_GENERIC_INOUT = 4,
-  REPORT_ID_COUNT
+	INTERFACE_KEYBOARD,
+	INTERFACE_MOUSE,
+	INTERFACE_GAMEPAD,
+	INTERFACE_RAWHID,
+	ITF_NUM_TOTAL
 };
+
+enum
+{
+	STRID_LANGID = 0,
+	STRID_MANUFACTURER,
+	STRID_PRODUCT,
+	STRID_SERIAL,
+	STRID_HID_KEYBOARD,
+	STRID_HID_MOUSE,
+	STRID_HID_GAMEPAD,
+	STRID_HID_RAWHID,
+};
+
+//--------------------------------------------------------------------+
+// External Declarations
+//--------------------------------------------------------------------+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	extern uint8_t const desc_hid_keyboard[];
+	extern uint8_t const desc_hid_mouse[];
+	extern uint8_t const desc_hid_gamepad[];
+	extern uint8_t const desc_hid_rawhid[];
+
+	extern const char *string_desc_arr[];
+
+	uint8_t const *tud_descriptor_device_cb(void);
+	uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf);
+	uint8_t const *tud_descriptor_configuration_cb(uint8_t index);
+	uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* USB_DESCRIPTORS_H_ */
